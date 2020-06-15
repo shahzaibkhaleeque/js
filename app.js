0465fccbef14ba39900279c8f1ec3f0375a2a4db
shahzaibkhaleeque <64401470+shahzaibkhaleeque@users.noreply.github.com>
2020-06-15 23:09:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,130 @@
+/////chapter 1 assignemnt 1 
+
+var a = prompt("Error! please enter a valid password");
+var a = alert("Welcome to js Land...Happy coding!");
+var a = alert("Welcome to js Land");
+var a = alert("Happy coding!");
+var a = alert("hello i can run JS through my web browser's console");
+
+/////chapter 2 assignemnt 2
+
+var username;
+var MyName = "shahzaib khan";
+var age = 21;
+var c = alert(MyName);
+var d = alert(age);
+var f = alert("certified mobile application development");
+var a = alert("pizza");
+var b = alert("pizz");
+var q = alert("piz");
+var q = alert("pi");
+var y = alert("p");
+var email = alert("my email address is example@example.com");
+var show = alert("im trying to learn from the book a smarter way to learn Javascript");
+var main = document.write("yah!!! i can write content through javaScript ");
+var b = alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” ");
+
+/////chapter 3 assignemnt 3
+var age1 = alert("i am 21 years old");
+var visit = alert("you have visited this  site 14 times");
+var birth = 1999;
+
+var date = document.write(birth);
+
+var date1 = document.write("date type of my decklard variable is number");
+
+var john = document.write(" “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”. ");
+
+/////////chapter 4 assignemnt 4
+document.write("Rules for naming JS variable");
+
+document.write(" Variable names can only contain ____number__, __$____, _  For example $my_1stVariable");
+
+document.write("Variables must begin with a ______, ______ or _____. For example $name, _name or nam");
+document.write("Rules for naming JS variable");
+document.write("Variable names are case _________");
+document.write(" Variable names should not be JS ________");
+
+/////////chapter 5 assignemnt 5
+
+var num = 5;
+var num1 = 6;
+
+var total = num + num1;
+document.write(total);
+var open = alert(" Write a program that take two numbers & add them in a new variable. Show the result in your browser. 2. Repeat task1 for subtraction, multiplication, division & modulus .3.Do the following using JS Mathematic Expressions a.Declare a variable.b.Show the value of variable in your browser like“ Value after variable declaration is: ? ? ”.c.Initialize the variable with some number.d.Show the value of variable in your browser like“ Initial value : 5”.e.Increment the variable.f.Show the value of variable in your browser like“ Value after increment is : 6”.g.Add 7 to the variable.h.Show the value of variable in your browser like“ Value MATH EXPRESSIONS | JAVASCRIPT Page 2 of 9 after addition is: 13”.i.Decrement the variable.j.Show the value of variable in your browser like“ Value after decrement is: 12”.k.Show the remainder after dividing the variable’ s value by 3. l.Output:The remainder is ");
+document.write(" Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets to a movie. Example output: ");
+document.write(" Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets to a movie. Example output:   MATH EXPRESSIONS | JAVASCRIPT  Page 3 of 9  5. Write a script to display multiplication table of any number in your browser. E.g   MATH EXPRESSIONS | JAVASCRIPT  Page 4 of 9  6. The Temperature Converter: It’s hot out! Let’s make a converter based on the steps here. a.  Store a Celsius temperature into a variable. b. Convert it to Fahrenheit & output “NNoC is NNoF”. c. Now store a Fahrenheit temperature into a variable. d. Convert it to Celsius & output “NNoF is NNoC”. Conversion Formulae:   7.  Write a program to implement checkout process of a shopping cart system for an e-commerce website. Store the following in variables MATH EXPRESSIONS | JAVASCRIPT  Page 5 of 9  a. Price of item 1 b. Price of item 2 c. Ordered quantity of item 1 d. Ordered Quantity of item 2 e. Shipping charges Compute the total cost & show the receipt in your browser.  8. Store total marks & marks obtained by a student in 2 variables. Compute the percentage & show the result in your browser     MATH EXPRESSIONS | JAVASCRIPT  Page 6 of 9   9. Assume we have 10 US dollars & 25 Saudi Riyals. Write a script to convert the total currency to Pakistani Rupees. Perform all calculations in a single expression. (Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee and 1 Saudi Riyal = 28 Pakistani Rupee)  MATH EXPRESSIONS | JAVASCRIPT Page 7 of 9  10. Write a program to initialize a variable with some number and do arithmetic in following sequence: a. Add 5 b. Multiply by 10 c. Divide the result by 2 Perform all calculations in a single expression  11. The Age Calculator: Forgot how old someone is? Calculate it! a. Store the current year in a variable. b. Store their birth year in a variable. c. Calculate their 2 possible ages based on the stored values. Output them to the screen like so: “They are either NN or NN years old”.  12. The Geometrizer: Calculate properties of a circle. a. Store a radius into a variable. MATH EXPRESSIONS | JAVASCRIPT  Page 8 of 9b. Calculate the circumference based on the radius, and output “The circumference is NN”. (Hint : Circumference of a circle = 2 π r , π = 3.142) Calculate the area based on the radius, and output “The  area is NN”. (Hint : Area of a circle = π r2, π = 3.142) 13. The Lifetime Supply Calculator: Ever wonder how much a “lifetime supply” of your favorite snack is? Wonder no more. a. Store your favorite snack into a variable b. Store your current age into a variable. c. Store a maximum age into a variable. d. Store an estimated amount per day (as a number). e. Calculate how many would you eat total for the rest of your life. Output the result to the screen like so: “You will need NNNN to last you until the ripe old age of NN");
+
+
+
+/////////chapter 6 assignemnt 6 - 9
+var p = promt("enter a value");
+for (var i = 0; i < 100; i++) {
+
+    document.write("The value of a++");
+    document.write("The value of a--");
+    document.write("The value of ++a");
+    document.write("The value of --a");
+
+}
+
+
+var eng = "english";
+var ma = "math";
+var u = "urdu";
+
+var num1 = 54;
+var num2 = 54;
+var num3 = 48;
+
+var pe1 = "54%";
+var pe2 = "54%";
+var pe2 = "54%";
+
+document.write(eng, ma, u, 54, 54, 48, pe1, p0e2, pe3);
+
+///////////////chapter 9 assignemnt 9 - 11
+
+
+var a = 4;
+if (++a === 5) { alert("given condition for variable a is true"); }
+
+
+var b = 82;
+if (b++ === 83) { alert("given condition for variable b is true"); }
+var c = 12;
+if (c++ === 13) { alert("condition 1 is true"); }
+if (c === 13) { alert("condition 2 is true"); }
+if (++c < 14) { alert("condition 3 is true"); }
+if (c === 14) { alert("condition 4 is true"); }
+var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost) { alert("The cost equals"); }
+if (true) { alert("True"); }
+if (false) { alert("False"); }
+if ("car" < "cat") { alert("car is smaller than cat"); }
+
+/////////chapter 12 assignemnt 12-13
+
+var greeting;
+var hour = 13;
+if (hour < 18) { greeting = "Good day"; } else { greeting = "Good evening"; }
+
+/////////chapter 14 assignemnt 14-16
+
+var arr = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. phil", "PHD"];
+document.write(arr);
+
+var arr1 = ["karachi", "peshawar", "islamabad", "quetta", "balochistan", "punjab", "lahore", "KPK"];
+document.write(arr1);
+
+/////////chapter 17 assignemnt 17-20
+
+
+var king = promt(enter a number);
+for (var i = king; i < 100; i++) {
+
+    document.write(i);
+}
